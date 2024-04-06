b39a2908666d6a94d8c62354740cc77020b0a709
Arseney <119616999+2twenity@users.noreply.github.com>
2024-04-06 22:17:01 +0000

    Stream and commit selection from Speckle was implemented

--- app.py
+++ app.py
@@ -1,49 +1,87 @@
 from viktor import ViktorController
-from viktor import UserMessage, progress_message
-from viktor.parametrization import ViktorParametrization, NumberField, ColorField, TextField, IntegerField, TextAreaField, OutputField, Image, ActionButton, Text, Field, OptionField
+from viktor import UserMessage, progress_message, UserError
+from viktor.parametrization import ViktorParametrization, NumberField, ColorField, TextField, IntegerField, TextAreaField, OutputField, Image, ActionButton, Text, Field, OptionField, Step, OptionListElement
 from viktor.geometry import SquareBeam, Color, Material, Group, LinearPattern, Point, Sphere
 from viktor.views import GeometryResult, GeometryView, DataView, DataResult, DataGroup, DataItem
 from viktor.result import DownloadResult
 
-from speckle_worker import pull_stream_names
+from specklepy.api.client import SpeckleClient
+from specklepy.api.credentials import get_account_from_token
+
+def pull_stream_names(params, **kwargs):
+    client = SpeckleClient(host=params.step_1.user_speckle_server_url)
+    account = get_account_from_token(token=params.step_1.user_token,
+                                     server_url=params.step_1.user_speckle_server_url)
+    client.authenticate_with_account(account=account)
+    data = [(stream.name, stream.id) for stream in client.stream.list()]
+
+    #params.stream_map = {i[0]: i[1] for i in data.items()}
+
+    names = [i[0] for i in data]
+    return names
+
+def pull_commits(params, **kwargs):
+    client = SpeckleClient(host=params.step_1.user_speckle_server_url)
+    account = get_account_from_token(token=params.step_1.user_token,
+                                     server_url=params.step_1.user_speckle_server_url)
+    client.authenticate_with_account(account=account)
+    data = {stream.name: stream.id for stream in client.stream.list()}
+    #stream = client.stream.get(STREAM_ID)
+    #print(client.commit.list(stream_id=STREAM_ID))
+
+    return [commit.message for commit in client.commit.list(stream_id=data[params.step_2.selected_stream])] 
+
+def validate_step_1(params, **kwargs):
+    if len(params.step_1.user_token) != 42:
+        raise UserError("The length of token is bad")
 
 class Parametrization(ViktorParametrization):
-    intro = Text("RegenBuild v. 0.1 pulls data from Speckle ")
-    user_token = TextField("Enter you Speckle Token", default=None)
-    user_speckle_server_url = TextField("Enter Speckle sever URL, default is https://app.speckle.systems", default="https://app.speckle.systems")
-    if user_token is not None:
-        streams_data = pull_stream_names(token=user_token, host=user_speckle_server_url)
-        #streams_names = list(streams_data.keys())
-        print("\nDown to here\n")
-        selected_stream = OptionField("Speckle streams from your account", options=streams_data, default="Stream 1")
+
+    #First Step
+    step_1 = Step('Step 1 - Speckle data input', on_next=validate_step_1)
+    step_1.intro = Text("Insert Speckle token with required permissions and if needed specify the Speckle seerver URL")
+    step_1.user_token = TextField("Enter you Speckle Token")
+    step_1.user_speckle_server_url = TextField("Enter Speckle sever URL, default is https://app.speckle.systems", 
+                                               default="https://app.speckle.systems")
+
+    #Second Step
+    step_2 = Step('Step 2 - Speckle stream selection')
+    step_2.intro = Text("Choose one of the streams")
+    step_2.selected_stream = OptionField("Available Speckle streams from your account", 
+                                         options=pull_stream_names)
+
+    #Third Step
+    step_3 = Step('Step 3 - Speckle commit selection')
+    step_3.selected_commit = OptionField("Available commits in the selected stream",
+                                          options=pull_commits)
 
 class Controller(ViktorController):
     label = 'Repoting'
     parametrization = Parametrization
     text = Text("Here we are")
 
-    # @GeometryView('3D Geometry', duration_guess=1, x_axis_to_right=True)
-    # def get_3d_view(self, params, **kwargs):
-    #     geometry = Sphere(Point(0, 0, 0), radius=params.number)
-    #     UserMessage.info("Dimensions were changed")
-    #     return GeometryResult(geometry)
+    @GeometryView('3D Geometry', duration_guess=1, x_axis_to_right=True)
+    def get_3d_view(self, params, **kwargs):
+        geometry = Sphere(Point(0, 0, 0), radius=params.number)
+        UserMessage.info("Dimensions were changed")
+        return GeometryResult(geometry)
     
-    # @DataView('Data', duration_guess=1)
-    # def get_data_view(self, params, **kwargs):
-    #     addition = params.x + params.y
-    #     multiplication = params.x * params.y
-    #     text = Text("Here we are")
+    @DataView('Data', duration_guess=1)
+    def get_data_view(self, params, **kwargs):
+        addition = params.x + params.y
+        multiplication = params.x * params.y
+        text = Text("Here we are")
 
-    #     main_data_group = DataGroup(
-    #         DataItem('Data item 1', addition),
-    #         DataItem('Data item 2', multiplication),
-    #         DataItem('Some text', text)
-    #     )
+        main_data_group = DataGroup(
+            DataItem('Data item 1', addition),
+            DataItem('Data item 2', multiplication),
+            DataItem('Some text', text)
+        )
 
-    #     return DataResult(main_data_group)
+        return DataResult(main_data_group)
 
-    # def download_file(self, params, **kwargs):
-    #     return DownloadResult('file content', 'file name')
+    def download_file(self, params, **kwargs):
+        return DownloadResult('file content', 'file name')
     
-    # def perform_action(self, params, **kwargs):
-    #     return OutputField("info")
+    def perform_action(self, params, **kwargs):
+        return OutputField("info")
--- speckle_worker.py
+++ speckle_worker.py
@@ -2,19 +2,3 @@ from specklepy.api.client import SpeckleClient
 from specklepy.api.client import SpeckleClient
 from specklepy.api.credentials import get_account_from_token
 from specklepy.api.wrapper import StreamWrapper
-
-HOST = "https://app.speckle.systems"
-TOKEN = "08edb0eea2f618aac0f3c5ef166605fcef34ce3f68"
-StreamID2 = "0cf5412fbe"
-NewLink = "https://app.speckle.systems/projects/0cf5412fbe/models/604bc1bedc"
-
-client = SpeckleClient(host=HOST)
-account = get_account_from_token(TOKEN, HOST)
-client.authenticate_with_account(account=account)
-
-def pull_stream_names(token, host="https://app.speckle.systems"):
-    data = [(stream.name, stream.id) for stream in client.stream.list()]
-    names = [i[0] for i in data]
-    return names
-
-#print(list(pull_stream_names(token="08edb0eea2f618aac0f3c5ef166605fcef34ce3f68").keys()))
