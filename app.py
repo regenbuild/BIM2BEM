be9c893d7f52f5b145b8e429b3e6540429eaed08
Arseney <119616999+2twenity@users.noreply.github.com>
2024-04-13 13:59:15 +0000

    Branch selection added. We can test topology creation from app now

--- app.py
+++ app.py
@@ -8,28 +8,55 @@ from viktor.result import DownloadResult
 from specklepy.api.client import SpeckleClient
 from specklepy.api.credentials import get_account_from_token
 
+from topologicpy.Speckle import Speckle
+
+def pull_client(params, **kwargs):
+    """Temporary solution to get authentificated client
+    There must be a way to cache authentificated client in viktor"""
+    client = SpeckleClient(host=params.step_1.user_speckle_server_url)
+    account = get_account_from_token(token=params.step_1.user_token,
+                                     server_url=params.step_1.user_speckle_server_url)
+    client.authenticate_with_account(account=account)
+    return client
+
 def pull_stream_names(params, **kwargs):
-    client = SpeckleClient(host=params.step_1.user_speckle_server_url)
-    account = get_account_from_token(token=params.step_1.user_token,
-                                     server_url=params.step_1.user_speckle_server_url)
-    client.authenticate_with_account(account=account)
-    data = [(stream.name, stream.id) for stream in client.stream.list()]
+    """Pulls stream names from Speckle server for user to choose"""
+    client = pull_client(params=params)
+    return [stream.name for stream in client.stream.list()]
 
-    #params.stream_map = {i[0]: i[1] for i in data.items()}
-
-    names = [i[0] for i in data]
-    return names
-
-def pull_commits(params, **kwargs):
-    client = SpeckleClient(host=params.step_1.user_speckle_server_url)
-    account = get_account_from_token(token=params.step_1.user_token,
-                                     server_url=params.step_1.user_speckle_server_url)
-    client.authenticate_with_account(account=account)
+def pull_stream_itself(params, **kwargs):
+    """Pulls stream object from Speckle server according to selection choice from user"""
+    client = pull_client(params=params)
     data = {stream.name: stream.id for stream in client.stream.list()}
-    #stream = client.stream.get(STREAM_ID)
-    #print(client.commit.list(stream_id=STREAM_ID))
+    return client.stream.get(data[params.step_2.selected_stream_name])
 
-    return [commit.message for commit in client.commit.list(stream_id=data[params.step_2.selected_stream])] 
+def pull_commits_names(params, **kwargs):
+    """Pulls commits names from Speckle server for user to select"""
+    client = pull_client(params=params)
+    data = {stream.name: stream.id for stream in client.stream.list()}
+    return [commit.message for commit in client.commit.list(stream_id=data[params.step_2.selected_stream_name])]
+
+def pull_commit_itself(params, **kwargs):
+    """Pulls commit object from Speckle server according to the user choice"""
+    client = pull_client(params=params)
+    data_stream = {stream.name: stream.id for stream in client.stream.list()}
+    data_commit = {commit.message: commit.id for commit in client.commit.list(stream_id=data_stream[params.step_2.selected_stream_name])}
+    return client.commit.get(stream_id=data_stream[params.step_2.selected_stream_name],
+                             commit_id=data_commit[params.step_3.selected_commit_name])
+
+def pull_branches_names(params, **kwargs):
+    """Pulls branches names for user to select"""
+    client = pull_client(params=params)
+    data = {stream.name: stream.id for stream in client.stream.list()}
+    return [branch.name for branch in client.branch.list(stream_id=data[params.step_2.selected_stream_name])]
+
+def pull_branch_itself(params, **kwargs):
+    """Pulls branch object from Speckle according to the selected parameters"""
+    client = pull_client(params=params)
+    #data_branch = {branch.name: branch.id for branch in client.branch.list()}
+    data_stream = {stream.name: stream.id for stream in client.stream.list()}
+    return client.branch.get(stream_id=data_stream[params.step_2.selected_stream_name],
+                             name=params.step_3.selected_branch_name)
 
 def validate_step_1(params, **kwargs):
     if len(params.step_1.user_token) != 42:
@@ -37,45 +64,66 @@ def validate_step_1(params, **kwargs):
 
 class Parametrization(ViktorParametrization):
 
-    #First Step
+    #Speckle credentials Step
     step_1 = Step('Step 1 - Speckle data input', on_next=validate_step_1)
     step_1.intro = Text("Insert Speckle token with required permissions and if needed specify the Speckle seerver URL")
     step_1.user_token = TextField("Enter you Speckle Token")
     step_1.user_speckle_server_url = TextField("Enter Speckle sever URL, default is https://app.speckle.systems", 
                                                default="https://app.speckle.systems")
 
-    #Second Step
+    #Stream selection step
     step_2 = Step('Step 2 - Speckle stream selection')
     step_2.intro = Text("Choose one of the streams")
-    step_2.selected_stream = OptionField("Available Speckle streams from your account", 
+    step_2.selected_stream_name = OptionField("Available Speckle streams from your account", 
                                          options=pull_stream_names)
 
-    #Third Step
-    step_3 = Step('Step 3 - Speckle commit selection')
-    step_3.selected_commit = OptionField("Available commits in the selected stream",
-                                          options=pull_commits)
+    #Commit and branch selection step
+    step_3 = Step('Step 3 - Speckle commit selection and Branch selection')
+    step_3.intro = Text("Choose commit and branch")
+    step_3.selected_commit_name = OptionField("Available commits in the selected stream",
+                                          options=pull_commits_names)
+    step_3.selected_branch_name = OptionField("Available branches",
+                                             options=pull_branches_names)
+    
+    step_4 = Step('Step 4 - Energy simulation parametrization', views="get_data_view")
+    step_4.intro = Text("Soup iz semi zalup")
 
 class Controller(ViktorController):
     label = 'Repoting'
     parametrization = Parametrization
     text = Text("Here we are")
 
-    @GeometryView('3D Geometry', duration_guess=1, x_axis_to_right=True)
-    def get_3d_view(self, params, **kwargs):
-        geometry = Sphere(Point(0, 0, 0), radius=params.number)
-        UserMessage.info("Dimensions were changed")
-        return GeometryResult(geometry)
+    # @GeometryView('3D Geometry', duration_guess=1, x_axis_to_right=True)
+    # def get_3d_view(self, params, **kwargs):
+    #     geometry = Sphere(Point(0, 0, 0), radius=params.number)
+    #     UserMessage.info("Dimensions were changed")
+    #     return GeometryResult(geometry)
     
     @DataView('Data', duration_guess=1)
     def get_data_view(self, params, **kwargs):
-        addition = params.x + params.y
-        multiplication = params.x * params.y
-        text = Text("Here we are")
+        # addition = params.x + params.y
+        # multiplication = params.x * params.y
+        # text = Text("Here we are")
+        stream = pull_stream_itself(params=params)
+        commit = pull_commit_itself(params=params)
+        branch = pull_branch_itself(params=params)
+        client = SpeckleClient(host=params.step_1.user_speckle_server_url)
+        account = get_account_from_token(token=params.step_1.user_token,
+                                     server_url=params.step_1.user_speckle_server_url)
+        client.authenticate_with_account(account=account)
+
+        topologic_obj_flag = None
+        try:
+            topologic_obj = Speckle.Object(client=client, stream=stream, branch=branch, commit=commit)
+            topologic_obj_flag = True
+        except:
+            topologic_obj_flag = False
 
         main_data_group = DataGroup(
-            DataItem('Data item 1', addition),
-            DataItem('Data item 2', multiplication),
-            DataItem('Some text', text)
+            DataItem('Testing data from stream', stream.id),
+            DataItem('Testing data from commit', commit.id),
+            DataItem('Testing data from branch', branch.name),
+            DataItem('Topologic obj', str(topologic_obj_flag))
         )
 
         return DataResult(main_data_group)
--- topologic_worker.py
+++ topologic_worker.py
@@ -1,3 +1,56 @@
-from topologicpy.Topology import Topology
-from topologicpy.Speckle import Speckle
+from specklepy.api.client import SpeckleClient
+from specklepy.api.credentials import get_account_from_token
+from specklepy.transports.server import ServerTransport
+from specklepy.api.wrapper import StreamWrapper
+from specklepy.api import operations
+from specklepy.objects.geometry import Mesh, Point, Polyline
 
+# from topologicpy.Topology import Topology
+# from topologicpy.Speckle import Speckle
+
+# HOST = "https://app.speckle.systems/"
+
+# Rhino = {"STREAM_ID": "8e8d1d72f2", "BRANCH_ID": "f0f78ec5b5", 
+#          "COMMIT_ID": "08c438b716", "BRANCH_NAME": "base design"}
+
+# Revit = {"STREAM_ID": "1ee6b67f72", "BRANCH_ID": "'95a09c4e56'", 
+#          "COMMIT_ID": "7bb81e4454", "BRANCH_NAME": "main"}
+
+# Blender = {"STREAM_ID": "c6c6e4e829", "BRANCH_ID": "9c691752ef", 
+#          "COMMIT_ID": "b11a6fd0ad", "BRANCH_NAME": "main"}
+
+# client = SpeckleClient(host=HOST)
+# account = get_account_from_token(token=TOKEN, server_url=HOST)
+# client.authenticate_with_account(account=account)
+
+# # print(client.stream.list())
+
+# # print(client.branch.list(Blender["STREAM_ID"]))
+# # print(client.commit.list(Blender["STREAM_ID"]))
+
+# stream = client.stream.get(Revit["STREAM_ID"])
+# commit = client.commit.get(commit_id=Revit["COMMIT_ID"], stream_id=Revit["STREAM_ID"])
+# branch = client.branch.get(stream_id=Revit["STREAM_ID"], name=Revit["BRANCH_NAME"])
+
+# # print(branch_panelka)
+
+# # obj = Speckle.Object(client=client, stream=stream_panelka, branch=branch_panelka, commit=commit_panelka)
+# # print(obj)
+
+# transport = ServerTransport(stream_id=stream.id, client=client)
+# last_obj_id = commit.referencedObject
+# speckle_mesh = operations.receive(obj_id=last_obj_id, remote_transport=transport)
+
+# data = speckle_mesh['elements']
+
+# # for each in data:
+# #     print(each)
+
+# first_data = data[0]
+# first_el = first_data.elements[0]
+# print(dir(first_el))
+# # print(first_el.parameters)
+
+# # first = data[0]
+# # first_el = first.elements[0]
+# # print(first_el.elementId)
